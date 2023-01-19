c32bda0434206094c46728f622af14ceecf37e5d
Jan-Ole Busch <janole.busch@gmail.com>
2023-01-19 22:03:29 +0100

    refactor: Change folder structure

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import '../css/style.css'
+import javascriptLogo from '../media/javascript.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
